51fcf137763593cc931361eb9bbfe4897b44c01b
Tevin <Stratmon.jones@gmail.com>
2023-11-17 20:30:37 -0600

    Initial Commit

---
+++ Barcode-Scanner-SwiftUI/Barcode_Scanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_Scanner_SwiftUIApp.swift
+//  Barcode-Scanner-SwiftUI
+//
+//  Created by Tevin Jones on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_Scanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
