5f3f3f683e288b08e9be5b9d73f443d42e422d9c
Tevin <Stratmon.jones@gmail.com>
2023-11-17 21:11:31 -0600

    initial commit

---
+++ Barcode-Scanner-SwiftUI/BarcodeScannerView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Barcode-Scanner-SwiftUI
+//
+//  Created by Tevin Jones on 11/17/23.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 400)
+                Spacer().frame(height:80)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text("Not yet scanned")
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
